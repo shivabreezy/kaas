fd098a57d7fea03e4c19c0774a798b7a80c692a7
Shiva <shivabreezy@gmail.com>
2018-10-04 18:22:07 +0200

    First and probably last commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+document.getElementsByClassName('menu-toggle')[0].onclick = function () {
+	var menu = document.getElementById('menu');
+	if(menu.className == "active"){
+		menu.className = "inactive";
+	} else {
+		menu.className = "active";
+	}
+}
